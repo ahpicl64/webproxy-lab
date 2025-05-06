810c337e46c68b8b6fc9b49dd968fcb01a4821dc
ahpicl64 <ubuntu@ip-172-31-2-144.ap-northeast-2.compute.internal>
2025-05-07 02:19:49 +0900

    feat : tiny parse_uri function complete

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -115,3 +115,30 @@ void read_requesthdrs(rio_t *rp) {
     }
     return;
 }
+
+int parse_uri(char *uri, char *filename, char *cgiargs){
+    char *ptr;
+
+    if (!strstr(uri, "cgi-bin")) { // static content
+        strcpy(cgiargs, "");
+        strcpy(filename, ".");
+        strcat(filename, uri);
+        if (uri[strlen(uri)-1] == '/'){
+            strcat(filename, "home.html");
+        }
+        return 1;
+    }
+    else { // dynamic content
+        ptr = index(uri, '?');
+        if (ptr) {
+            strcpy(cgiargs, ptr+1);
+            *ptr = '\0';
+        }    
+        else {
+            strcpy(cgiargs, "");
+        }
+        strcpy(filename, ".");
+        strcat(filename, uri);
+        return 0;
+    }
+}
